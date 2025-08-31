fa96b2f2dae8eae2bc4b8399a74d4dd9bfa072b9
F-jirayut <fayristh0268@gmail.com>
2025-08-31 14:42:37 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Jobs\ProcessPodcast;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    ProcessPodcast::dispatch();
     return view('welcome');
 });
